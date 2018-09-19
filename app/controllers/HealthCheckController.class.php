57c0e7fe4c60999f4b3f9715aee17d41a4c56ef0
peppernwine <corporate@peppernwine.com>
2018-09-19 00:41:29 -0400

    fixed health check

--- app/controllers/HealthCheckController.class.php
+++ app/controllers/HealthCheckController.class.php
@@ -8,19 +8,7 @@
 
 class HealthCheckController
 {
-    public function init()
-    {
-        session_start();
-
-        if (!isset($_SESSION['healthCheckCounter'])) {
-            $_SESSION['healthCheckCounter'] = 1;
-        } else {
-            $_SESSION['healthCheckCounter']++;
-        }
-
-        if ($_SESSION['healthCheckCounter'] > 3) {
-            http_response_code(404);
-    } else
-            http_response_code(200);
+    public function init(){
+        http_response_code(200);
     }
 }
