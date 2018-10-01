0fdc0ae2d87169ac990c3f490f8acbbb2f5aa48b
peppernwine <corporate@peppernwine.com>
2018-09-30 21:25:03 -0400

    added AWS route auto update during EC2 instance bootup

--- docker-startup.sh
+++ docker-startup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 #remove if the log files are a SYMLINK like SYMLINK to /dev/stdout
-if [ -h /var/log/apache2/access.log ]; then
-  #rm /var/log/apache2/access.log
+if [ -h /var/log/apache2/access.log ]
+  then
+  rm /var/log/apache2/access.log
 fi
 
 #remove if the log files are a SYMLINK like SYMLINK to /dev/stdout
-if [ -h /var/log/apache2/error.log ]; then
-  #rm /var/log/apache2/error.log
+if [ -h /var/log/apache2/error.log ]
+then
+  rm /var/log/apache2/error.log
 fi
 
 mkdir -p /var/log/apache2/
