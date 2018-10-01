c1d3920120505294fee288eff8d0d9085195962d
peppernwine <corporate@peppernwine.com>
2018-09-30 22:31:03 -0400

    added AWS route auto update during EC2 instance bootup

--- docker-startup.sh
+++ docker-startup.sh
@@ -17,4 +17,4 @@ mkdir -p /var/log/apache2/
 #ln -s /log/apache2/access.log /var/log/apache2/access.log
 #ln -s /log/apache2/error.log /var/log/apache2/error.log
 
-apache2-foreground
+exec apache2-foreground
